6c9b644d6c4abbd97bdda2a2dfc920b54e879752
Camila <camila.ogliastri.00@gmail.com>
2022-11-18 12:34:43 +0100

    :tada:

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'soft-red': 'hsl(7, 99%, 70%)',
+        'yellow': 'hsl(51, 100%, 49%)',
+        'dark-desaturated-cyan': 'hsl(167, 40%, 24%)',
+        'dark-blue': 'hsl(198, 62%, 26%)',
+        'dark-moderate-cyan': 'hsl(168, 34%, 41%)',
+        'very-dark-desaturated-blue': 'hsl(212, 27%, 19%)',
+        'very-dark-grayish-blue': 'hsl(213, 9%, 39%)',
+        'dark-grayish-blue': 'hsl(232, 10%, 55%)',
+        'grayish-blue': 'hsl(210, 4%, 67%)',
+      },
+      fontfamily: {
+        barlow: ['Barlow', 'sans-serif'],
+        fraunces: ['Fraunces', 'serif'], 
+      },
+      backgroundImage: {
+        'orang' : "url('../Images/image-gallery-orange.jpg')",
+        'egg' : "url('../Images/image-transform.jpg')",
+        'pinkglass' : "url('Images/image-stand-out.jpg')",
+        'cherry' : "url('Images/image-graphic-design.jpg')",
+        'mandarine' : "url('Images/image-photography.jpg')",
+      },
+    },
+  },
+  plugins: [],
+}
